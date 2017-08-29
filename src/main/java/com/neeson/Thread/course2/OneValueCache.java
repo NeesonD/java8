2bf9280dbc065c457959f8094b186c9af52f2a0c
neeson <d1043358774@gmail.com>
2017-08-29 10:41:30 +0800

    新增并发编程实战第三章笔记

---
+++ src/main/java/com/neeson/Thread/course2/OneValueCache.java
@@ -0,0 +1,25 @@
+package com.neeson.Thread.course2;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+/**
+ * Created by daile on 2017/8/29.
+ */
+public class OneValueCache {
+
+    private final BigInteger lastNumber;
+    private final BigInteger[] lastFactors;
+
+    public OneValueCache(BigInteger lastNumber, BigInteger[] lastFactors) {
+        this.lastNumber = lastNumber;
+        this.lastFactors = Arrays.copyOf(lastFactors,lastFactors.length);
+    }
+
+    public BigInteger[] getLastFactors(BigInteger i){
+        if (lastNumber == null||!lastNumber.equals(i))
+            return null;
+        else
+            return Arrays.copyOf(lastFactors,lastFactors.length);
+    }
+}
---
+++ src/main/java/com/neeson/Thread/course2/UnsafeStatus.java
@@ -0,0 +1,18 @@
+package com.neeson.Thread.course2;
+
+/**
+ * Created by daile on 2017/8/28.
+ */
+public class UnsafeStatus {
+
+
+    //status逸出了自己的作用域
+    private String[] status = new String[]{
+            "a","b"
+    };
+
+    public String[] getStatus(){
+        return status;
+    }
+
+}
