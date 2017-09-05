4b4505d75b6dcfbccc4dc3120f9815ccbb21f6ac
neeson <d1043358774@gmail.com>
2017-09-05 22:37:35 +0800

    增加并发编程第7章实例

---
+++ src/main/java/com/neeson/Thread/course7/BrokenPrimeProducer.java
@@ -0,0 +1,52 @@
+package com.neeson.Thread.course7;
+
+import java.math.BigInteger;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+
+/**
+ * Create by neeson on 2017/9/5
+ */
+public class BrokenPrimeProducer extends Thread {
+    
+    private final BlockingQueue<BigInteger> queue;
+    
+    private volatile boolean cancelled = false;
+
+    public BrokenPrimeProducer(BlockingQueue<BigInteger> queue) {
+        this.queue = queue;
+    }
+    
+    public void run(){
+        try {
+            BigInteger p = BigInteger.ONE;
+            while (!cancelled){
+                queue.put(p = p.nextProbablePrime());
+            }
+        }catch (InterruptedException e){
+            
+        }
+    }
+    
+    public void cancell(){
+        cancelled = true;
+    }
+    
+    void comsumePrimes() throws InterruptedException {
+        BlockingQueue<BigInteger> primes = new ArrayBlockingQueue<BigInteger>(1);
+        
+        BrokenPrimeProducer producer = new BrokenPrimeProducer(primes);
+        producer.start();
+        try {
+            while (true){
+                consume(primes.take());
+            }
+        }finally {
+            producer.cancell();
+        }
+    }
+
+    private void consume(BigInteger take) {
+    }
+}
