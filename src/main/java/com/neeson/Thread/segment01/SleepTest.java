5f23bd01691504062cc0c1f7f88793259cdf2900
neeson <d1043358774@gmail.com>
2018-03-29 15:13:43 +0800

    添加多线程demo

---
+++ src/main/java/com/neeson/Thread/segment01/SleepTest.java
@@ -0,0 +1,32 @@
+package com.neeson.Thread.segment01;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: neeson
+ * Date: 2018/3/19
+ * Time: 11:00
+ * Description:
+ */
+public class SleepTest {
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Mythread1 thread = new Mythread1();
+        Thread thread1 = new Thread(thread);
+        thread1.start();
+        Thread.sleep(3000);
+        System.out.println("===>"+Thread.currentThread().getName());
+    }
+
+    static class Mythread1 implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println(">>>"+Thread.currentThread().getName());
+        }
+
+    }
+
+}
